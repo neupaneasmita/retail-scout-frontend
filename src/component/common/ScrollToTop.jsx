b5e5e7b10400b59dea354961756d690e47689fa1
asmitaneupane <neupane.ashmita2056@gmail.com>
2021-10-24 22:41:51 +0545

    scroll to top component

---
+++ src/component/common/ScrollToTop.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { withRouter } from "react-router-dom";
+
+function ScrollToTop({ history }) {
+  useEffect(() => {
+    const unListen = history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+    return () => {
+      unListen();
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return null;
+}
+
+export default withRouter(ScrollToTop);
